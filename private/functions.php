c5a1ab57feb66dda5d7a256c01ddd2ab250e7589
Abdifatah Hassan <abdi.hassannoor01@gmail.com>
2022-02-02 22:36:19 +0200

    created functions to encode dynamic data

--- private/functions.php
+++ private/functions.php
@@ -1,7 +1,7 @@
 
 <?php
 
-function setUrlPath($script_path)
+function setUrlPath(string $script_path)
 {
 
     if ($script_path[0] != '/') {
@@ -11,3 +11,17 @@ function setUrlPath($script_path)
     return WWW_ROOT . $script_path;
 }
 
+function eUrl(string $string)
+{
+    return urlencode($string);
+}
+
+function dUrl(string $string)
+{
+    return urldecode($string);
+}
+
+function htmlChars(string $string)
+{
+    return htmlspecialchars($string);
+}

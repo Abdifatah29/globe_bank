ce50f52b712711137ce4eeeee5f1426b611adeec
Abdifatah Hassan <abdi.hassannoor01@gmail.com>
2022-03-10 22:53:27 +0200

    database connection

---
+++ private/database.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'db_credentials.php';
+
+function db_connect()
+{
+        $conn = mysqli_connect(HOST, USERNAME, PASSWORD, DATABASE );
+
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+          }
+          
+        return $conn;
+}
+
+
+function db_close($conn)
+{
+    return isset($conn) ? mysqli_close($conn) : $conn;
+}
+
---
+++ private/db_credentials.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+-----------------------------------
+        DATABASE CONNECTION
+-----------------------------------
+*/
+
+define('HOST', '127.0.0.1');
+define('DATABASE', 'globe_bank');
+define('USERNAME', 'root');
+define('PASSWORD', 'root');
